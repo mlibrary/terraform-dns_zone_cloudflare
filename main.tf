1f9cbd2f1c91a171585e0f2dfae1cbe68f855b24
Ryan Rotter <rrotter@users.noreply.github.com>
2023-07-20 18:33:27 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,53 @@
+data "cloudflare_zone" "zone" {
+  name = var.zone
+}
+
+resource "cloudflare_record" "txt" {
+  for_each = local.txt_records_map
+  zone_id  = data.cloudflare_zone.zone.id
+  type  = "TXT"
+  name  = each.value.name
+  value = each.value.target
+  ttl   = 1 # this means use the Cloudflare default, not literally one second
+}
+
+resource "cloudflare_record" "mx" {
+  for_each = var.mx_records
+  zone_id  = data.cloudflare_zone.zone.id
+  type     = "MX"
+  name     = each.value.name
+  value    = each.value.target
+  priority = each.value.priority
+  ttl      = 3600
+}
+
+resource "cloudflare_record" "ns" {
+  for_each = local.ns_records_map
+  zone_id  = data.cloudflare_zone.zone.id
+  type     = "NS"
+  name     = each.value.name
+  value    = each.value.target
+  ttl      = 3600
+}
+
+resource "cloudflare_record" "cname" {
+  for_each = local.cname_records_map
+  zone_id  = data.cloudflare_zone.zone.id
+  type  = "CNAME"
+  name  = each.value.name
+  value = each.value.target
+  ttl   = 1
+}
+
+resource "cloudflare_record" "a" {
+  for_each = local.a_records_map
+  zone_id  = data.cloudflare_zone.zone.id
+  type  = "A"
+  name  = each.value.name
+  value = each.value.target
+  ttl   = 1
+}
+
+output zone_id {
+  value = data.cloudflare_zone.zone.id
+}
---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "zone" {
+  description = "name of zone"
+  type = string
+}
+
+variable "txt_records" {
+  description = "Map of TXT records. Keys are the record names, values are String or String[] containing record data."
+  type = any
+  default = {}
+}
+
+variable "mx_records" {
+  description = "Map of MX records. Keys are just titles, values is expected to be a map containing name, target, and priority."
+  type = map
+  default = {}
+}
+
+variable "ns_records" {
+  description = "Map of NS records. Keys are the record names, values are String or String[] containing record data."
+  type = any
+  default = {}
+}
+
+variable "a_records" {
+  description = "Map of A records. Keys are the record names, values are String or String[] containing record data."
+  type = any
+  default = {}
+}
+
+variable "cname_records" {
+  description = "Map of CNAME records. Keys are the record names, values are String or String[] containing record data."
+  type = any
+  default = {}
+}
+
+variable "cnames_many_to_one" {
+  description = "REVERSE map of CNAME records. Keys are the record *data*, values are String or String[] containing record name. This allows for very terse creation of many CNAMEs with the same target."
+  type = any
+  default = {}
+}
